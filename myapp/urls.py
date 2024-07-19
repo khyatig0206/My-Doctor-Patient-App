aa7f67deebda813a42270a27fbc8dd520f6cb593
Khyati Gupta <khyatig0206@gmail.com>
2024-07-19 16:22:33 +0530

    first commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from .views import login_page,register_page,dashboard,logout_page
 
 urlpatterns = [
     path('login/',login_page,name='login_page'),
-    path('register/',register_page,name='register_page'),
+    path('',register_page,name='register_page'),
     path('logout/',logout_page,name='logout_page'),
     path('dashboard/',dashboard,name='dashboard')
 ]
